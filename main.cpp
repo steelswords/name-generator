7ee2d024cb46a3f9f142a7b8dfd598732538c69d
Tristan Andrus <tristan@tristanandrus.com>
2022-04-10 22:01:53 -0600

    Add command-line argument to specify grammar file
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
+#include <filesystem>
 #include <iostream>
 #include <string>
 #include "TextGenerationGrammar.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-  TextGenerationGrammar generator("inns.dat");
+  if (argc != 2)
+  {
+    std::cerr << "Usage: " << std::string(argv[0]) << " <grammar.dat file>"
+      << std::endl;
+    return 1;
+  }
+  // Check if passed in file actually exists.
+  std::string grammarFile(argv[1]);
+  std::filesystem::path filePath {grammarFile};
+  if (!std::filesystem::exists(filePath))
+  {
+    std::cerr << "ERROR! " << filePath << " does not exist!" << std::endl;
+    return 2;
+  }
+
+  TextGenerationGrammar generator(grammarFile);
   std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << "-------------------------------" << std::endl;
   generator.clearState();
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
-  std::cout << generator.generate() << std::endl;
   return 0;
 }
